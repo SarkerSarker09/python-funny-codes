a53819edf0b454312ca5dcfce3fabcb08997607c
samer sarker <samerseu@gmail.com>
2021-01-16 15:25:23 +0600

    updated source added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # python-funny-codes
 Python Funny Codes Tricks
+
+### List Reversing
+### List values assigned varriable dynamically
+### Summation and loop in a line
+### Print string N times
+### Join many iterator object list 
+
+
+
+
+
+
---
+++ index.py
@@ -0,0 +1,35 @@
+# List Reversing
+MyList = ['Banana', 'Grape', 'Orange']
+MyList.reverse()
+
+print(MyList)
+
+
+# List values assigned varriable dynamically
+NumbersList = [1, 3, 5, 7 ,9]
+x, y, z, a , b = NumbersList
+print(x, y ,z)
+print(a,b)
+
+
+# Summation and loop in a line
+t = sum(item for item in range(10))
+print(t)
+
+#Print string N times
+str = "Hello"
+print (str *3)
+
+
+
+#Join many iterator object list 
+Year = (2001, 2003, 2004)
+Month = ('Oct', 'Nov', 'Dec')
+Day = (10, 11, 12)
+print allInOne = (zip(Year, Month, Day))
+print(allInOne)
+
+
+
+
+
