98a377bb1ca3f5f326b584c0913fa4974ac23a13
Samer Sarker Khokon <54855806+SarkerSarker09@users.noreply.github.com>
2021-02-25 18:00:36 +0600

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,30 @@
+
+# start code block2021 start
+def one():
+    print("one function")
+    
+def two():
+    print("two function")
+    
+def test():
+    one()
+    two()
+    print("hello world!")
+
+if __name__ == "__main__":
+    test()
+        
+# end code block2021    
+
+#code block is for run your code automatically when run this file with command: python index.py
+
+
+
+
+
+
+
+
 # List Reversing
 MyList = ['Banana', 'Grape', 'Orange']
 MyList.reverse()
